2d98614ea44bace944dadde0192ff315047ac90a
kylieanyce <kylieanyce@gmail.com>
2021-05-27 13:15:41 -0500

    added reports for games by user

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -8,3 +8,4 @@ class Game(models.Model):
     max_players = models.IntegerField()
     difficulty = models.CharField(max_length=50)
     game_type = models.ForeignKey("GameType", on_delete=models.CASCADE)
+    gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
